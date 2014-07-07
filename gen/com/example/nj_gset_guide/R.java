e2c1b552ecf7bd98880f661ff0e124ad8c2b8c60
Gordon Chu <c.gordo97@gmail.com>
2014-07-07 17:58:04 -0400

    Removed unnecessary search bar - to be implemented later

--- gen/com/example/nj_gset_guide/R.java
+++ gen/com/example/nj_gset_guide/R.java
@@ -23,9 +23,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int default_menu_item=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
+        public static final int action_search=0x7f080006;
         public static final int action_settings=0x7f080005;
         public static final int container=0x7f080000;
         public static final int listView1=0x7f080004;
@@ -44,14 +46,15 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int action_search=0x7f050003;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int explore_msg=0x7f050003;
+        public static final int explore_msg=0x7f050004;
         public static final int hello_world=0x7f050001;
-        public static final int menu_item1=0x7f050004;
-        public static final int menu_item2=0x7f050005;
-        public static final int menu_item3=0x7f050006;
-        public static final int title_activity_descriptions=0x7f050007;
+        public static final int menu_item1=0x7f050005;
+        public static final int menu_item2=0x7f050006;
+        public static final int menu_item3=0x7f050007;
+        public static final int title_activity_descriptions=0x7f050008;
     }
     public static final class style {
         /** 
