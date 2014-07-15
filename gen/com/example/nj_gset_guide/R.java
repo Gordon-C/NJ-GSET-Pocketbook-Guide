ae2ad1f6082a7e869ad1358fd33e6a578b49aa45
Gordon Chu <c.gordo97@gmail.com>
2014-07-15 18:17:42 -0400

    created textview

--- gen/com/example/nj_gset_guide/R.java
+++ gen/com/example/nj_gset_guide/R.java
@@ -30,14 +30,15 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
+        public static final int action_settings=0x7f090008;
         public static final int container=0x7f090000;
-        public static final int iv_building_img=0x7f090005;
-        public static final int lv_places=0x7f090004;
-        public static final int tvDescription=0x7f090002;
-        public static final int tvTitle=0x7f090001;
-        public static final int tv_building_name=0x7f090006;
-        public static final int tvwelcome=0x7f090003;
+        public static final int iv_building_img=0x7f090006;
+        public static final int lv_places=0x7f090005;
+        public static final int tvDescription=0x7f090003;
+        public static final int tvTitle=0x7f090002;
+        public static final int tv_building_name=0x7f090007;
+        public static final int tv_desc=0x7f090001;
+        public static final int tvwelcome=0x7f090004;
     }
     public static final class layout {
         public static final int activity_descriptions=0x7f030000;
